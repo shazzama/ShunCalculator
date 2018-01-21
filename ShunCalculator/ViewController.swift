e1bb0ebdcdfe13065a9b7d12e7a70459906bf597
Shazzama <georgenjoo@yahoo.com>
2018-01-20 23:31:53 -0800

    Initial Commit

---
+++ ShunCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShunCalculator
+//
+//  Created by George Njoo on 2018-01-20.
+//  Copyright © 2018 Njoko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
